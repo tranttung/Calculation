5ee254d45104a0ead3d8ccb29f0bc84492851d19
tungtt1 <tungtt1@runsystem.net>
2024-06-24 10:08:17 +0700

    create-app-2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Basic Calculator 123</h1>
+        <h1>Basic Calculator</h1>
         <Calculator />
       </header>
     </div>
